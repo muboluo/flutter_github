29b6d36d79b80c8393f6f16cc75cd19ff8ecf24f
zhangheng24 <zhangheng24@58.com>
2021-08-10 19:53:28 +0800

    imp,login_page, 添加部分 页面布局

--- lib/common/style/styles.dart
+++ lib/common/style/styles.dart
@@ -6,6 +6,9 @@ class ZHStyle {
   static const Color actionBlue = Color(0xff267aff);
   static const Color black = Color(0xff000000);
   static const Color primaryColor = Color(0xff121917);
+  static const Color cardWhite = Color(0xFFFFFFFF);
+}
 
-
+class ZHIcons {
+  static const String DEFAULT_USER_ICON = "static/images/logo.png";
 }
--- lib/routes/login/login_page.dart
+++ lib/routes/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_github/common/style/styles.dart';
 
 class LoginPage extends StatefulWidget {
   static const PAGE_NAME = "LoginPage";
@@ -12,14 +13,61 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        child: Stack(
-          children: [
-            Center(
-              child: Text("login page"),
-            )
-          ],
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      //  scaffold 是脚手架，作为每个页面的跟布局
+      //  布局思维是从外到内的，一层层的去实现。
+      child: Scaffold(
+        // todo 为什么这里是 Container?
+        body: Container(
+          color: Theme.of(context).primaryColor,
+          // 因为界面中包括三个层级：背景渐变动画/ 圆球上移动画 / 中间卡片内容区域
+          // 所以用了 stack 层级布局来实现。类似于 android的帧布局
+          child: Stack(
+            children: [
+              //卡片是屏幕居中的
+              Center(
+                child: SafeArea(
+                  // 防止 overflow现象，什么是overflow现象？https://www.jianshu.com/p/5d3654c39c34
+                  child: SingleChildScrollView(
+                    child: Card(
+                      elevation: 5.0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.0))),
+                      color: ZHStyle.cardWhite,
+                      margin: EdgeInsets.only(left: 30.0, right: 30.0),
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            left: 30.0, top: 40.0, right: 30.0, bottom: 0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Image(
+                                image:
+                                    new AssetImage(ZHIcons.DEFAULT_USER_ICON),
+                                width: 90.0,
+                                height: 90.0),
+                            Padding(padding: EdgeInsets.all(100)),
+                            // todo 输入widget name
+                            Padding(padding: EdgeInsets.all(10.0)),
+                            // todo 输入 widget pwd
+
+
+
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
